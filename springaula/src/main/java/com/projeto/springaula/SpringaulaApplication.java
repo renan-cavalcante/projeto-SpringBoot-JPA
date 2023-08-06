247af0cb6095081fcc5efa495b734219faedcab5
renan-cavalcante <renancggama@gmail.com>
2023-08-06 12:46:39 -0300

    first commit

---
+++ springaula/src/main/java/com/projeto/springaula/SpringaulaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.springaula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaulaApplication.class, args);
+	}
+
+}
