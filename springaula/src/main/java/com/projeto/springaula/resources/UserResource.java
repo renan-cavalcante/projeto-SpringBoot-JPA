d1d2486b52baf29849af67d2aa79fbef8a947759
renan-cavalcante <renancggama@gmail.com>
2023-08-21 14:44:38 -0300

    criando camada de serviço

--- springaula/src/main/java/com/projeto/springaula/resources/UserResource.java
+++ springaula/src/main/java/com/projeto/springaula/resources/UserResource.java
@@ -1,19 +1,35 @@
 package com.projeto.springaula.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projeto.springaula.entities.User;
+import com.projeto.springaula.services.userService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	
+	@Autowired
+	private userService service;
 
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User user = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.finadAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User user = service.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
+		
+	
 }
