9480dcac8c33e63df0485bf3a1ec5043c98add9c
renan-cavalcante <renancggama@gmail.com>
2023-08-28 16:25:53 -0300

    Criando enum orderStatus

--- springaula/src/main/java/com/projeto/springaula/config/TestConfig.java
+++ springaula/src/main/java/com/projeto/springaula/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.projeto.springaula.entities.Order;
 import com.projeto.springaula.entities.User;
+import com.projeto.springaula.entities.enums.OrderStatus;
 import com.projeto.springaula.repositories.OrderRepository;
 import com.projeto.springaula.repositories.UserRepository;
 
@@ -31,9 +32,9 @@ public class TestConfig implements CommandLineRunner {
 		User u2 = new User(null, "Pedro gama", "pedrogama@gmail.com", "977774444", "123456");
 	
 
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1); 
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.DELIVERED, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.DELIVERED, u1); 
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
